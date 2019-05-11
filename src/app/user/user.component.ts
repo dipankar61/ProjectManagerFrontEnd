1a7bfdd64a81a27fe9e7349e536ac70465c23d1d
dipankar61 <dipankar61@gmail.com>
2019-05-11 03:03:14 -0400

    user component partial html and code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {Observable} from "rxjs/index";
+import { Http, Request, RequestOptions, RequestOptionsArgs, Response, XHRBackend } from "@angular/http";
+import { error } from '@angular/compiler/src/util';
+import {UserService} from '../user.service';
+import{User} from '../model/user';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  userForm:FormGroup;
+  model:User;
+  successMsg: string;
+  errorMsg: string;
+  isSuccess:boolean=false;
+  isError:boolean=false;
+  isEdit:boolean=false;
 
-  constructor() { }
+  constructor(private userService:UserService,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    
+    this.userForm = this.formBuilder.group({
+      firstName: new FormControl('',Validators.required) ,
+      lastName: new FormControl('',Validators.required),
+      empId:new FormControl('',[Validators.pattern("^[0-9]*$"),Validators.required])
+    });
   }
+  get f() { return this.userForm.controls; }
+  onSubmit(){
+    
+
+  }
+  ResetForm(){
+    this.isError=false;
+    this.isSuccess=false;
+    this.isEdit=false;
+    this.userForm.reset();
+  }
+  
 
 }
